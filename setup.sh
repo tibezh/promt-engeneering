1d14ebd3120d5030839310bc44250e970c601fb8
Ivan Tibezh <ivan.tibezh@jobiqo.com>
2025-04-21 13:15:16 +0300

    Added requirements and setup.sh script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create a virtual env.
+python3 -m venv promt_engineering_env
+
+# Activate virtual env.
+source promt_engineering_env/bin/activate
+
+# Install dependencies.
+pip install -r requirements.txt
+
+echo "The env has been configured. To activate run: source promt_engineering_env/bin/activate"
